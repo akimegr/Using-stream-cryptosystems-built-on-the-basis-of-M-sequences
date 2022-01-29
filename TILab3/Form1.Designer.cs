f06095beacd099f931fcf003f94269e6c4e62c86
akimegr <82463845+akimegr@users.noreply.github.com>
2022-01-29 16:07:22 +0300

    feat: add files

---
+++ TILab3/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace TILab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(51, 718);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(578, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(502, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter shift register length";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(599, 134);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(431, 55);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "intial value in register";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(627, 68);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 62);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(627, 193);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(360, 62);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox5.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox5.Location = new System.Drawing.Point(51, 303);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox5.Size = new System.Drawing.Size(519, 169);
+            this.textBox5.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(43, 244);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(245, 55);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Cyphertext:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox7.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox7.Location = new System.Drawing.Point(51, 68);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox7.Size = new System.Drawing.Size(519, 171);
+            this.textBox7.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox6.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox6.Location = new System.Drawing.Point(51, 541);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox6.Size = new System.Drawing.Size(519, 169);
+            this.textBox6.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(376, 718);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 67);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(43, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 55);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Plaintext:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(41, 482);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(202, 55);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Plaintext:";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(1054, 68);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(169, 232);
+            this.checkedListBox1.TabIndex = 20;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 53;
+            this.listBox1.Location = new System.Drawing.Point(627, 362);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(292, 269);
+            this.listBox1.TabIndex = 21;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(617, 303);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(284, 55);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "register states";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(1265, 798);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Encoder-decoder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ TILab3/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TILab3
+{
+    public partial class Form1 : Form
+    {
+        
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            byte byte1 = 4;
+            textBox1.Text = System.Convert.ToString(byte1);
+
+            textBox7.Text = "Hello world!!!";
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)  // проверка ввода
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                    e.Handled = true;
+            }
+        }
+ 
+        private void button1_Click(object sender, EventArgs e)  // обработчик нажатия кнопки шифрования
+        {
+            if (listBox1.Items.Count != 0)
+            {
+                int count = listBox1.Items.Count;   
+                int bitsCount = Convert.ToInt32(textBox1.Text) - 1;
+                int[] keys = new int[count];    // инициализация для создания int-ых ключей
+                for (int i = 0; i < count; i++)  //цикл для прохода по каждому ключу
+                {
+                    keys[i] = 0;
+
+                    int j = 0;
+                    foreach (char letter in listBox1.Items[i].ToString())   // цикл для перевода двоичного ключа из листБокса в int
+                    {
+                        if (letter == '1')
+                        {
+                            keys[i] += 1 * (int)Math.Pow(2, (bitsCount - j));
+                        }
+                        else
+                        {
+                            keys[i] += 0;
+                        }
+                        j++;
+                    }
+                }
+                int k = 0;
+                textBox5.Text = "";
+                foreach (char letter in textBox7.Text)  // цикл для шифрования каждого символа посредством xor на соответствующий ключ
+                {
+                    textBox5.Text += Convert.ToChar(letter ^ keys[k % count]);
+                    k++;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)  // обработчик нажатия кнопки дешифрования (всё точь в точь, как у шифрования)
+        {
+            if (listBox1.Items.Count != 0)
+            {
+
+                int count = listBox1.Items.Count;
+                int bitsCount = Convert.ToInt32(textBox1.Text) - 1;
+                int[] keys = new int[count];
+                for (int i = 0; i < count; i++)
+                {
+                    keys[i] = 0;
+
+                    int j = 0;
+                    foreach (char letter in listBox1.Items[i].ToString())
+                    {
+                        if (letter == '1')
+                        {
+                            keys[i] += 1 * (int)Math.Pow(2, (bitsCount - j));
+                        }
+                        else
+                        {
+                            keys[i] += 0;
+                        }
+                        j++;
+                    }
+                }
+                int k = 0;
+                textBox6.Text = "";
+                foreach (char letter in textBox5.Text)
+                {
+                    textBox6.Text += Convert.ToChar(letter ^ keys[k % count]);
+                    k++;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)   // обработчик изменения длины ключа
+        {
+            int num = Convert.ToInt32(textBox1.Text); 
+            if(num > 8) // проверка соответствия длины диапазону
+            {
+                textBox1.Text = "8";
+                num = 8;
+            }
+            else if(num < 4)
+            {
+                textBox1.Text = "4";
+                num = 4;
+            }
+
+            textBox2.Text = "";
+            checkedListBox1.Items.Clear();
+
+            for(int i = 0; i < num; i++)    // создание соответствующиего кол-ва переключателей
+            {
+                checkedListBox1.Items.Add($"Bit {i + 1}");
+                textBox2.Text += "" + rnd.Next(0, 2);
+            }
+
+            int[] indexes = { -1, -1, -1, -1 };
+            switch (num)    // инициализация индексов для генератора ключей (по методичке)
+            {
+                case 4:
+                    {
+                        indexes[0] = 0;
+                        indexes[1] = 3;
+                        break; 
+                    }
+                case 5:
+                    {
+                        indexes[0] = 1;
+                        indexes[1] = 4;
+                        break;
+                    }
+                case 6:
+                    {
+                        indexes[0] = 0;
+                        indexes[1] = 5;
+                        break;
+                    }
+                case 7:
+                    {
+                        indexes[0] = 0;
+                        indexes[1] = 5;
+                        break;
+                    }
+                case 8:
+                    {
+                        indexes[0] = 0;
+                        indexes[1] = 4;
+                        indexes[2] = 5;
+                        indexes[3] = 7;
+                        break;
+                    }
+                    
+            }
+            for(int i = 0; i < 4; i++)  // включение соответствующих переключателей
+            {
+                if (indexes[i] != -1)
+                {
+                    checkedListBox1.SetItemChecked(indexes[i], true);
+                }
+                else
+                    break;
+            }
+            checkedListBox1.SelectedIndex = indexes[0]; // для активации ивента
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)   // обработчик изменения кол-ва переключателей
+        {
+            int bitsCount = Convert.ToInt32(textBox1.Text);
+            if (bitsCount != 8) // проверка на длину ключа, т.к. в 8-битном используются 4 переключателя вместо 2-ух
+            {
+                if (checkedListBox1.CheckedItems.Count > 2) // отключение переключателей, если их больше 2-ух
+                {
+                    for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                        checkedListBox1.SetItemChecked(i, false);
+                    checkedListBox1.SetItemChecked(checkedListBox1.SelectedIndex, true);
+                }
+                else if (checkedListBox1.CheckedItems.Count == 2) // генерация ключей, если 2 переключателя активны
+                {
+                    listBox1.Items.Clear();
+
+                    byte[] bits = new byte[bitsCount];
+                    int i = 0;
+                    foreach (char num in textBox2.Text) // перевод строки в последовательность бит
+                    {
+                        if (num == '1')
+                        {
+                            bits[i] = 1;
+                        }
+                        else
+                        {
+                            bits[i] = 0;
+                        }
+                        i++;
+                    }
+
+
+                    listBox1.Items.Add((listBox1.Items.Count+1).ToString() + " " + textBox2.Text);
+
+                    byte newBit = Convert.ToByte(bits[checkedListBox1.CheckedIndices[0]] ^ bits[checkedListBox1.CheckedIndices[1]]); // создание нового бита с помощью xor указанных переключателями бит
+
+                    string key = "";    // строковое представление двоичного ключа
+                    for (i = 0; i < bits.Length - 1; i++) // цикл для смещения битов в сторону большего разряда
+                    {
+                        bits[i] = bits[i + 1];
+                        key += bits[i] % 2;
+                    }
+                    bits[bits.Length - 1] = newBit; // добавления нового бита в младший разряд
+                    key += newBit;
+
+                    for (i = 0; i < Math.Pow(2, bitsCount)+3; i++)
+                    {
+                        listBox1.Items.Add((listBox1.Items.Count+1).ToString() + " " + key);    // добавление ключа в список
+                        newBit = Convert.ToByte(bits[checkedListBox1.CheckedIndices[0]] ^ bits[checkedListBox1.CheckedIndices[1]]); // создание нового бита с помощью xor указанных переключателями бит
+
+                        key = "";
+                        for (int j = 0; j < bits.Length - 1; j++)   // цикл для смещения битов в сторону большего разряда
+                        {
+                            bits[j] = bits[j + 1];
+                            key += bits[j] % 2;
+                        }
+                        bits[bits.Length - 1] = newBit; // добавления нового бита в младший разряд
+                        key += newBit;
+                    }
+                }
+
+            }
+            else    // всё то же самое только с использованием 4 переключателей для 8-битного ключа
+            {
+                if (checkedListBox1.CheckedItems.Count > 4)
+                {
+                    for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                        checkedListBox1.SetItemChecked(i, false);
+                    checkedListBox1.SetItemChecked(checkedListBox1.SelectedIndex, true);
+                }
+                else if (checkedListBox1.CheckedItems.Count == 4)
+                {
+                    listBox1.Items.Clear();
+
+                    byte[] bits = new byte[bitsCount];
+                    int i = 0;
+                    foreach (char num in textBox2.Text)
+                    {
+                        if (num == '1')
+                        {
+                            bits[i] = 1;
+                        }
+                        else
+                        {
+                            bits[i] = 0;
+                        }
+                        i++;
+                    }
+
+
+                    listBox1.Items.Add(textBox2.Text);
+
+                    byte newBit = Convert.ToByte(bits[checkedListBox1.CheckedIndices[0]] ^ bits[checkedListBox1.CheckedIndices[1]] ^ bits[checkedListBox1.CheckedIndices[2]] ^ bits[checkedListBox1.CheckedIndices[3]]);
+
+                    string key = "";
+                    for (i = 0; i < bits.Length - 1; i++)
+                    {
+                        bits[i] = bits[i + 1];
+                        key += bits[i] % 2;
+                    }
+                    bits[bits.Length - 1] = newBit;
+                    key += newBit;
+
+                    for (i = 0; i < Math.Pow(2, bitsCount) - 2; i++)
+                    {
+                        listBox1.Items.Add(key);
+                        newBit = Convert.ToByte(bits[checkedListBox1.CheckedIndices[0]] ^ bits[checkedListBox1.CheckedIndices[1]] ^ bits[checkedListBox1.CheckedIndices[2]] ^ bits[checkedListBox1.CheckedIndices[3]]);
+
+                        key = "";
+                        for (int j = 0; j < bits.Length - 1; j++)
+                        {
+                            bits[j] = bits[j + 1];
+                            key += bits[j] % 2;
+                        }
+                        bits[bits.Length - 1] = newBit;
+                        key += newBit;
+                    }
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
